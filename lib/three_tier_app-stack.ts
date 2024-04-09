6fac68873f053a12fea348f20d301433f851001b
Eddie <pupueddy@gmail.com>
2024-04-09 01:27:57 -0300

    Initial commit

--- lib/three_tier_app-stack.ts
+++ lib/three_tier_app-stack.ts
@@ -1,16 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Vpc, InstanceType } from 'aws-cdk-lib/aws-ec2';
+import { Cluster, ContainerImage, FargateService, FargateTaskDefinition } from 'aws-cdk-lib/aws-ecs';
+import { DatabaseInstance, DatabaseInstanceEngine, PostgresEngineVersion } from 'aws-cdk-lib/aws-rds';
 
-export class ThreeTierAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+export class ThreeTierAppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC
+    const vpc = new Vpc(this, 'VPC');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThreeTierAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an ECS cluster
+    const cluster = new Cluster(this, 'Cluster', { vpc });
+
+    // Define a task definition with a single container
+    // The container image is a simple web app
+    const taskDefinition = new FargateTaskDefinition(this, 'TaskDef');
+    taskDefinition.addContainer('WebContainer', {
+      image: ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      memoryLimitMiB: 512,
+    });
+
+    // Create a Fargate service
+    new FargateService(this, 'Service', {
+      cluster,
+      taskDefinition,
+    });
+
+   // Create an RDS database
+new DatabaseInstance(this, 'Database', {
+  engine: DatabaseInstanceEngine.postgres({ version: PostgresEngineVersion.VER_13 }),
+  instanceType: new InstanceType('t3.micro'),
+  vpc,
+});
+
   }
 }
